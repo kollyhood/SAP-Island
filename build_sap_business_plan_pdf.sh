ac86a858ddaef29b41941fe73f54b9ccfce7c95f
kollyhood <sunder.sunder@gmail.com>
2026-03-10 13:47:05 +0530

    Update build_sap_business_plan_pdf.sh

--- build_sap_business_plan_pdf.sh
+++ build_sap_business_plan_pdf.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mkdir -p out
 
-pandoc sap_business_plan_v1_0.md \
+pandoc sap_business_plan_v1_2.md \
   -o out/sap_business_plan_v1_2.pdf \
   --pdf-engine=xelatex \
   --resource-path=.
